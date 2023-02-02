c63a9a4855c3c9585237cee2a82000e36511127c
Joe Jarvis <joe.jarvis109@gmail.com>
2023-02-01 20:48:15 -0500

    Don't bundle react

--- webpack.config.js
+++ webpack.config.js
@@ -6,77 +6,80 @@ const MiniCssExtrackPlugin = require("mini-css-extract-plugin");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-    entry: {
-        index: __dirname + "/src/index.tsx"
-    },
-    devtool: "source-map",
-    output: {
-        path: __dirname + "/dist",
-        filename: "[name].bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtrackPlugin.loader, "css-loader"]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ ".ts", ".tsx", ".js", ".jsx" ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtrackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "src/note_ui.html",
-            filename: "note_ui.html",
-            chunks: ["index"],
-        }),
-        new copyWebpackPlugin({
-            patterns: [
-                { from: "node_modules/quill/dist/quill.min.js", to: "third-party/quill.min.js" },
-                { from: "node_modules/quill/dist/quill.min.js.map", to: "third-party/quill.min.js.map" },
+module.exports = (env, argv) => {
+    const config = {
+        entry: {
+            index: __dirname + "/src/index.tsx"
+        },
+        devtool: "source-map",
+        output: {
+            path: __dirname + "/dist",
+            filename: "[name].bundle.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtrackPlugin.loader, "css-loader"]
+                }
             ]
-        })
-        // new copyWebpackPlugin({
-        //     patterns: [
-        //         { from: "src/manifest.json" },
-        //         {
-        //             from: "src/icons/",
-        //             to: "icons",
-        //             toType: "dir"
-        //         },
-        //         {
-        //             from: "src/_locales/",
-        //             to: "_locales",
-        //             toType: "dir"
-        //         },
-        //         { from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js" },
-        //         { from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js.map" }
-        //     ]
-        // })
-    ],
-    externals: {
-        "webextension-polyfill": "browser",
-        "quill": "Quill",
-        // "react": "React",
-        // "react-dom": "ReactDOM"
-    },
-    optimization: {
-        // splitChunks: {
-        //     chunks: "all"
-        // },
-        usedExports: true,
-        minimizer: [
-            new CssMinimizerWebpackPlugin(),
-            new TerserWebpackPlugin()
-        ]
+        },
+        resolve: {
+            extensions: [ ".ts", ".tsx", ".js", ".jsx" ],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtrackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "src/note_ui.html",
+                filename: "note_ui.html",
+                chunks: ["index"],
+            }),
+            new copyWebpackPlugin({
+                patterns: [
+                    { from: "node_modules/quill/dist/quill.min.js", to: "third-party/quill.min.js" },
+                    { from: "node_modules/quill/dist/quill.min.js.map", to: "third-party/quill.min.js.map" },
+                    { from: "node_modules/react/umd/" + (argv.mode === "production" ? "react.production.min.js" : "react.development.js"), to: "third-party/react.js"},
+                    { from: "node_modules/react-dom/umd/" + (argv.mode === "production" ? "react-dom.production.min.js" : "react-dom.development.js"), to: "third-party/react-dom.js"}
+                ]
+            })
+            // new copyWebpackPlugin({
+            //     patterns: [
+            //         { from: "src/manifest.json" },
+            //         {
+            //             from: "src/icons/",
+            //             to: "icons",
+            //             toType: "dir"
+            //         },
+            //         {
+            //             from: "src/_locales/",
+            //             to: "_locales",
+            //             toType: "dir"
+            //         },
+            //         { from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js" },
+            //         { from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js.map" }
+            //     ]
+            // })
+        ],
+        externals: {
+            "webextension-polyfill": "browser",
+            "quill": "Quill",
+            "react": "React",
+            "react-dom": "ReactDOM",
+        },
+        optimization: {
+            usedExports: true,
+            minimizer: [
+                new CssMinimizerWebpackPlugin(),
+                new TerserWebpackPlugin()
+            ]
+        }
     }
+
+    return config;
 }
