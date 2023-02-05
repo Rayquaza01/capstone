1fba9b003e616998b96340981a68215904075a10
Joe Jarvis <joe.jarvis109@gmail.com>
2023-02-05 11:51:37 -0500

    Dynamically set content

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,14 +1,28 @@
 import React, { MutableRefObject, useEffect, useRef } from "react";
 
-import Quill from "quill";
+import Quill, { TextChangeHandler } from "quill";
+
 require("quill/dist/quill.snow.css");
 
-export function Editor() {
+export interface EditorProps {
+    onChange?: TextChangeHandler
+    content: string
+}
+
+export function Editor(props: EditorProps) {
     const editor: MutableRefObject<null | Quill> = useRef(null);
 
     useEffect(() => {
         editor.current = new Quill("#editor", {theme: "snow"});
+        editor.current.focus();
+
+        if (props.onChange)
+            editor.current.on("text-change", props.onChange);
     }, []);
+
+    useEffect(() => {
+        editor.current?.setText(props.content, "api");
+    }, [props.content]);
 
     return (
         <div id="editor"></div>
