6be22255b4d3d3da4903bde468885142615d3f1a
Joe Jarvis <joe.jarvis109@gmail.com>
2023-04-03 09:55:45 -0400

    DB schema

---
+++ server/src/mysql.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE notetaker;
+
+CREATE USER 'notetaker'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT ALL PRIVILEGES ON notetaker.* TO 'notetaker'@'localhost';
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20),
+    password CHAR(60)
+);
+
+CREATE TABLE metadata (
+    id INT,
+    user_id INT,
+    parent INT,
+    name VARCHAR(20),
+    color CHAR(7),
+    type VARCHAR(10),
+    modified TIMESTAMP,
+    PRIMARY KEY (id, user_id)
+);
+
+CREATE TABLE data (
+    id INT,
+    user_id INT,
+    text TEXT CHARACTER SET utf8mb4,
+    modified TIMESTAMP,
+    PRIMARY KEY (id, user_id)
+);
