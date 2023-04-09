93cfa5b6fd5bdff50d2c85872a84b03c9cbd2879
Joe Jarvis <joe.jarvis109@gmail.com>
2023-04-09 19:36:26 -0400

    Setting up basic API

--- server/src/APITypes.ts
+++ server/src/APITypes.ts
@@ -1,3 +1,7 @@
+function isColor(color: string): boolean {
+    return (/#[a-fA-F0-9]{6}/).test(color);
+}
+
 export interface Entry {
     id: number
     parent: number
@@ -7,9 +11,26 @@ export interface Entry {
     modified: string | Date
 }
 
+export function isEntry(e?: Record<string, any>): e is Entry {
+    if (!e) return false;
+
+    return typeof e.id === "number" &&
+        typeof e.parent === "number" &&
+        (typeof e.name === "string" && e.name.length > 0) &&
+        (typeof e.color === "string" && isColor(e.color)) &&
+        typeof e.modified === "string";
+}
+
 export interface TextEntry {
     id: number,
     text: string
+}
+
+export function isTextEntry(e?: Record<string, any>): e is TextEntry {
+    if (!e) return false;
+
+    return typeof e.id === "number" &&
+        typeof e.text === "string";
 }
 
 export interface UpdateEntry {
@@ -17,6 +38,21 @@ export interface UpdateEntry {
     name?: string
     color?: string
     modified: string | Date
+}
+
+export function isUpdateEntry(e?: Record<string, any>): e is UpdateEntry {
+    if (!e) return false;
+
+    if ("name" in e) {
+        if (!(typeof e.name === "string")) return false;
+    }
+
+    if ("color" in e) {
+        if (!(typeof e.color === "string" && isColor(e.color))) return false;
+    }
+
+    return typeof e.id === "string" &&
+        typeof e.modified === "string";
 }
 
 export interface CreateAccountBody {
@@ -31,7 +67,14 @@ export interface AuthenticateBody {
 
 export interface CreateBody {
     auth: string
-    entry: Entry | Entry[]
+    entry: Entry[]
+}
+
+export function isCreateBody(e?: Record<string, any>): e is CreateBody {
+    if (!e) return false;
+
+    return typeof e.auth === "string" &&
+        (Array.isArray(e.entry) && e.entry.every(isEntry));
 }
 
 export interface UpdateBody {
@@ -39,9 +82,21 @@ export interface UpdateBody {
     entry: UpdateEntry
 }
 
+export function isUpdateBody(e?: Record<string, any>): e is UpdateBody {
+    if (!e) return false;
+
+    return typeof e.auth === "string" &&
+        isUpdateEntry(e.entry);
+}
+
 export interface DeleteBody {
     auth: string
-    id: number | number[]
+    id: number[]
+}
+
+export function isDeleteBody(e: Record<string, any>): e is DeleteBody {
+    return typeof e.auth === "string" &&
+        (Array.isArray(e.id) && e.id.every(i => typeof i === "number"));
 }
 
 export interface ListBody {
@@ -50,7 +105,27 @@ export interface ListBody {
     offset?: number
     limit?: number
     modifiedSince?: Date
-    type: "metadata" | "text" | "all"
+}
+
+export interface ListParentBody {
+    auth: string
+    parent: number
+    offset?: number
+    limit?: number
+}
+
+export interface ListModifiedBody {
+    auth: string
+    modifiedSince: number
+    offset?: number
+    limit?: number
+}
+
+export interface ListTextModifiedBody {
+    auth: string
+    modifiedSince: number
+    offset?: number
+    limit?: number
 }
 
 export interface GetTextBody {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-const app = express();
+import * as APITypes from "./APITypes";
+import * as DB from "./DB";
+import { authenticate } from "./authenticate";
 
-app.get("/", (req, res) => {
-    res.send("hello");
-});
+const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("hello");
@@ -13,20 +14,94 @@ app.get("/api", (req, res) => {
     res.send("hello");
 });
 
+app.post("/api/createAccount", (req, res) => {
+    res.send("hello");
+});
+
 app.post("/api/authenticate", (req, res) => {
     res.send("hello");
 });
 
-app.post("/api/create", (req, res) => {
+app.post("/api/create", async (req, res) => {
+    if (!APITypes.isCreateBody(req.body)) {
+        console.log(req.body);
+        res.status(400);
+        res.send({ error: "Request could not be understood" });
+        return;
+    }
+
+    // const jwt = authenticate(req.body.auth);
+    // if (!jwt) {
+    //     res.status(401);
+    //     res.send({ error: "Request could not be authenticated" });
+    //     return;
+    // }
+
+    // const dbQuery = await DB.createEntry(jwt.id, req.body.entry);
+    const dbQuery = await DB.createEntry(1, req.body.entry);
+    if (dbQuery.error) {
+        res.status(500);
+        res.send({ error: dbQuery.error });
+        return;
+    }
+
+    res.status(200);
+    res.send({ error: null });
+    return;
+});
+
+app.post("/api/update", async (req, res) => {
+    if (!APITypes.isUpdateBody(req.body)) {
+        res.status(400);
+        res.send({ error: "Request could not be understood" });
+        return;
+    }
+
+    const jwt = authenticate(req.body.auth);
+    if (!jwt) {
+        res.status(401);
+        res.send({ error: "Request could not be authenticated" });
+        return;
+    }
+
+    const dbQuery = await DB.updateEntry(jwt.id, req.body.entry);
+    if (dbQuery.error) {
+        res.status(500);
+        res.send({ error: dbQuery.error });
+        return;
+    }
+
+    res.status(200);
+    res.send({error: null});
+
     res.send("hello");
 });
 
-app.post("/api/update", (req, res) => {
-    res.send("hello");
-});
+app.post("/api/delete", async (req, res) => {
+    if (!APITypes.isDeleteBody(req.body)) {
+        res.status(400);
+        res.send({ error: "Request could not be understood" });
+        return;
+    }
 
-app.post("/api/delete", (req, res) => {
-    res.send("hello");
+    // const jwt = authenticate(req.body.auth);
+    // if (!jwt) {
+    //     res.status(401);
+    //     res.send({ error: "Request could not be authenticated" });
+    //     return;
+    // }
+
+    // const dbQuery = await DB.deleteEntry(jwt.id, req.body.id);
+    const dbQuery = await DB.deleteEntry(1, req.body.id);
+    if (dbQuery[0].error) {
+        res.status(500);
+        res.send({ error: dbQuery[0].error });
+        return;
+    }
+
+    res.status(200);
+    res.send({ error: null });
+    return;
 });
 
 app.post("/api/list", (req, res) => {
